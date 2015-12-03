4b3dbdcb2fb52043b1f8d4df1a3702d4e5319b73
Sean McGinnis <sean_mcginnis@dell.com>
2015-12-03 14:44:39 -0600

    Add reno for release notes management
    Adopt reno for release notes like we have done for cinder.
    One difference for the client is we will not be backporting
    to Liberty per recommendation here [1].
    
    [1] http://lists.openstack.org/pipermail/openstack-dev/2015-November/080694.html
    
    Change-Id: I8f0ea1e23cb60d9f5a2a41ee440c47716bb7a3f8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,8 +22,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'oslosphinx',
+    'reno.sphinxext',
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
