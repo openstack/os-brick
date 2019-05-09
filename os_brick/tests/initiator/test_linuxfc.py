70899a9aa3e4bdd3f1b1a7024807390cd55c4958
Gorka Eguileor <geguileo@redhat.com>
2019-05-09 17:25:03 +0200

    FC: Ignore some HBAs from map for single WWNN
    Current FC OS-Brick code only checks for single WWNN to exclude some
    HBAs from scanning when we don't receive an initiator_target_map from
    the backend.
    
    There are storage arrays,like XtremIO, that due to their architecture
    and design have all target ports automatically mapped to LUNs and are
    returning the initiator_target_map, but some of those ports may not be
    connected to our HBAs, so we end up with another case of bug #1765000.
    
    This patch makes sure that we always check if the target implements
    single WWNN, not only when we don't receive the initiator_target_map.
    
    With this we decrease the likelihood of ending with unexpected devices
    in our system, because now we will ignore unconnected HBAs (even if
    reported in the initiator_target_map) if we are working with single WWNN
    target.
    
    Related-Bug: #1765000
    Closes-Bug: #1828440
    Change-Id: I02c208142f5b342f894666831449243863eccbfe

--- os_brick/tests/initiator/test_linuxfc.py
+++ os_brick/tests/initiator/test_linuxfc.py
@@ -295,6 +295,33 @@ class LinuxFCTestCase(base.TestCase):
                               mock.call(hbas[1], con_props)]
             mock_get_chan.assert_has_calls(expected_calls)
 
+    def test_rescan_hosts_initiator_map_single_wwnn(self):
+        """Test FC rescan with initiator map and single WWNN."""
+        get_chan_results = [[['2', '3', 1], ['4', '5', 1]], [['-', '-', 1]]]
+
+        hbas, con_props = self.__get_rescan_info(zone_manager=True)
+
+        with mock.patch.object(self.lfc, '_execute',
+                               return_value=None) as execute_mock, \
+            mock.patch.object(self.lfc, '_get_hba_channel_scsi_target',
+                              side_effect=get_chan_results) as mock_get_chan:
+
+            self.lfc.rescan_hosts(hbas, con_props)
+            expected_commands = [
+                mock.call('tee', '-a', '/sys/class/scsi_host/host6/scan',
+                          process_input='2 3 1',
+                          root_helper=None, run_as_root=True),
+                mock.call('tee', '-a', '/sys/class/scsi_host/host6/scan',
+                          process_input='4 5 1',
+                          root_helper=None, run_as_root=True)]
+
+            execute_mock.assert_has_calls(expected_commands)
+            self.assertEqual(len(expected_commands), execute_mock.call_count)
+
+            expected_calls = [mock.call(hbas[0], con_props),
+                              mock.call(hbas[1], con_props)]
+            mock_get_chan.assert_has_calls(expected_calls)
+
     def test_rescan_hosts_wildcard(self):
         """Test when we don't have initiator map or target is single WWNN."""
         get_chan_results = [[['-', '-', 1]], [['-', '-', 1]]]
