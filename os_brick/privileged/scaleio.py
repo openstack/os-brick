5414305a2b2c776be62b695c4144dd4ace7f5b30
Ivan Pchelintsev <Ivan.Pchelintsev@dell.com>
2020-06-01 11:28:08 +0300

    Remove VxFlex OS credentials from connection_properties
    VxFlex OS password is not stored in block_device_mapping table. Instead of this
    passwords are stored in separate file and are retrieved during each attach/detach
    operation.
    
    Closes-Bug: #1823200
    Change-Id: Iab54c515fe7be252df52b1a0503a251779805759

--- os_brick/privileged/scaleio.py
+++ os_brick/privileged/scaleio.py
@@ -11,12 +11,14 @@
 #    under the License.
 
 from binascii import hexlify
+import configparser
 from contextlib import contextmanager
 from fcntl import ioctl
 import os
 import struct
 import uuid
 
+from os_brick import exception
 from os_brick import privileged
 
 SCINI_DEVICE_PATH = '/dev/scini'
@@ -70,3 +72,32 @@ def rescan_vols(op_code):
 
     with open_scini_device() as fd:
         ioctl(fd, op_code, struct.pack('Q', 0))
+
+
+@privileged.default.entrypoint
+def get_connector_password(filename, config_group, failed_over):
+    """Read ScaleIO connector configuration file and get appropriate password.
+
+    :param filename: path to connector configuration file
+    :type filename: str
+    :param config_group: name of section in configuration file
+    :type config_group: str
+    :param failed_over: flag representing if storage is in failed over state
+    :type failed_over: bool
+    :return: connector password
+    :rtype: str
+    """
+
+    if not os.path.isfile(filename):
+        msg = (
+            "ScaleIO connector configuration file "
+            "is not found in path %s." % filename
+        )
+        raise exception.BrickException(message=msg)
+
+    conf = configparser.ConfigParser()
+    conf.read(filename)
+    password_key = (
+        "replicating_san_password" if failed_over else "san_password"
+    )
+    return conf[config_group][password_key]
