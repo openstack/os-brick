e67b780db17b8c1e59f9198f243f174f50390387
Gorka Eguileor <geguileo@redhat.com>
2020-06-18 20:45:02 +0200

    RBD: Support non OpenStack usage
    The RBD connector can get a handle to a volume when we have the config
    and keyring file in the host and when all the information is provided by
    the Cinder driver (including the keyring).
    
    The code for the local attach (using krbd) does not work if all the
    information is provided by the Cinder driver, which is the case of
    cinderlib.
    
    This patch ensures that the local attach/detach behaves in the same way
    as when we request a handle.
    
    For security reasons, when doing a local attach for non-OpenStack usage
    the configuration file which contains the keyring will be owned by root
    instead of the normal user.
    
    Related-Bug: #1883720
    Change-Id: I315f474979ea5e797e3551cdc56ef136fa7a67c0

---
+++ os_brick/privileged/rbd.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2020, Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_utils import fileutils
+from oslo_utils import importutils
+
+import os_brick.privileged
+
+# Lazy load the rbd module to avoid circular references
+RBDConnector = None
+
+
+def _get_rbd_class():
+    global RBDConnector
+    global get_rbd_class
+
+    # Lazy load the class
+    if not RBDConnector:
+        rbd_class_route = 'os_brick.initiator.connectors.rbd.RBDConnector'
+        RBDConnector = importutils.import_class(rbd_class_route)
+
+    # Job is done, following calls don't need to do anything
+    get_rbd_class = lambda: None  # noqa
+
+
+get_rbd_class = _get_rbd_class
+
+
+@os_brick.privileged.default.entrypoint
+def delete_if_exists(path):
+    return fileutils.delete_if_exists(path)
+
+
+@os_brick.privileged.default.entrypoint
+def root_create_ceph_conf(monitor_ips, monitor_ports, cluster_name, user,
+                          keyring):
+    """Create a .conf file for Ceph cluster only accessible by root."""
+    get_rbd_class()
+    return RBDConnector._create_ceph_conf(monitor_ips, monitor_ports,
+                                          cluster_name, user, keyring)
