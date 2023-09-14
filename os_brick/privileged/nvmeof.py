ad860f6c4f247812f0aee85fce01c584ff637ab0
Gorka Eguileor <geguileo@redhat.com>
2023-09-14 18:38:59 +0200

    NVMe-oF: Improve hostnqn creation
    This patch improves the creation of the /etc/nvme/hostnqn file by using
    the system UUID value we usually already know.
    
    This saves us one or two calls to the nvme-cli command and it also
    allows older nvme-cli versions that don't have the `show-hostnqn`
    command or have it but can only read from file to generate the same
    value every time, which may be useful when running inside a container
    under some circumstances.
    
    Change-Id: Ib250d213295695390dbdbb3506cb297a86e95218
    (cherry picked from commit 088c676bf5d1846efc19385433a65c1675c2ebd1)
    (cherry picked from commit 83facbd9708742faa653f1603a9d813df85656a6)

--- os_brick/privileged/nvmeof.py
+++ os_brick/privileged/nvmeof.py
@@ -30,44 +30,54 @@ LOG = logging.getLogger(__name__)
 
 
 @os_brick.privileged.default.entrypoint
-def create_hostnqn() -> str:
+def create_hostnqn(system_uuid: Optional[str] = None) -> str:
     """Create the hostnqn file to speed up finding out the nqn.
 
     By having the /etc/nvme/hostnqn not only do we make sure that that value is
     always used on this system, but we are also able to just open the file to
     get the nqn on each get_connector_properties call instead of having to make
     a call to nvme show-hostnqn command.
+
+    In newer nvme-cli versions calling show-hostnqn will not only try to
+    locate the file (which we know doesn't exist or this method wouldn't have
+    been called), but it will also generate one.  In older nvme-cli versions
+    that is not the case.
     """
     host_nqn = ''
     try:
         os.makedirs('/etc/nvme', mode=0o755, exist_ok=True)
 
-        # Try to get existing nqn generated from dmi or systemd
-        try:
-            host_nqn, err = rootwrap.custom_execute('nvme', 'show-hostnqn')
-            host_nqn = host_nqn.strip()
+        # If we have the system's unique uuid we can just write the file
+        if system_uuid:
+            host_nqn = 'nqn.2014-08.org.nvmexpress:uuid:' + system_uuid
+        else:
+            # Try to get existing nqn generated from dmi or systemd
+            try:
+                host_nqn, err = rootwrap.custom_execute('nvme', 'show-hostnqn')
+                host_nqn = host_nqn.strip()
 
-        # This is different from OSError's ENOENT, which is missing nvme
-        # command.  This ENOENT is when nvme says there isn't an nqn.
-        except putils.ProcessExecutionError as e:
-            # nvme-cli's error are all over the place, so merge the output
-            err_msg = e.stdout + '\n' + e.stderr
-            msg = err_msg.casefold()
-            if 'error: invalid sub-command' in msg:
-                LOG.debug('Version too old cannot check current hostnqn.')
-            elif 'hostnqn is not available' in msg:
-                LOG.debug('Version too old to return hostnqn from non file '
-                          'sources')
-            elif e.exit_code == errno.ENOENT:
-                LOG.debug('No nqn could be formed from dmi or systemd.')
-            else:
-                LOG.debug('Unknown error from nvme show-hostnqn: %s', err_msg)
-                raise
+            # This is different from OSError's ENOENT, which is missing nvme
+            # command.  This ENOENT is when nvme says there isn't an nqn.
+            except putils.ProcessExecutionError as e:
+                # nvme-cli's error are all over the place, so merge the output
+                err_msg = e.stdout + '\n' + e.stderr
+                msg = err_msg.casefold()
+                if 'error: invalid sub-command' in msg:
+                    LOG.debug('Version too old cannot check current hostnqn.')
+                elif 'hostnqn is not available' in msg:
+                    LOG.debug('Version too old to return hostnqn from non '
+                              'file sources')
+                elif e.exit_code == errno.ENOENT:
+                    LOG.debug('No nqn could be formed from dmi or systemd.')
+                else:
+                    LOG.debug('Unknown error from nvme show-hostnqn: %s',
+                              err_msg)
+                    raise
 
-        if not host_nqn:
-            LOG.debug('Generating nqn')
-            host_nqn, err = rootwrap.custom_execute('nvme', 'gen-hostnqn')
-            host_nqn = host_nqn.strip()
+            if not host_nqn:
+                LOG.debug('Generating nqn')
+                host_nqn, err = rootwrap.custom_execute('nvme', 'gen-hostnqn')
+                host_nqn = host_nqn.strip()
 
         with open('/etc/nvme/hostnqn', 'w') as f:
             LOG.debug('Writing hostnqn file')
